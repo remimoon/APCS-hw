eef96d4b075705bce7b13165614b3a0ea52cef44
Mother <mother@Kyles-MacBook-Air.local>
2014-09-20 23:56:11 -0400

    added 02-greeter folder

---
+++ 02-greeter/greeter.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Greeter {
+
+    public void greet() {
+	System.out.println("Hello world!");
+    }
+
+    public void ungreet() {
+	System.out.println("I'm out.");
+    }
+
+}
