124fa4113749668dcfc49ef0404ecd13b1e61763
remim00n7@gmail.com <mother@Kyles-MacBook-Air.local>
2014-10-02 23:16:18 -0400

    hw 7b added

--- 07-shapes/Shapes.java
+++ 07-shapes/Shapes.java
@@ -1,5 +1,6 @@
 
 public class Shapes {
+  
     public String box(int r, int c) {
 	String s = "";
 
@@ -48,7 +49,7 @@ return s
 	int a = 1;
 	while (height>0){
 	    int stars = 1;
-	    int spaces = height-b;
+	    int spaces = height-stars;
 	    while (spaces > 0){
 		    s += " ";
 		    spaces --;
@@ -78,4 +79,78 @@ s=s+"\n"
 return s;
 }
     */
-}	   
+
+    public String tri3(int h){
+	String s="";
+	int row,spaces,stars;
+	// For row n, there are 1 + 2(n-1) stars
+	// For row n, there are a - n
+	for (row = 1; row <= h; row++){
+	    for (spaces=h-row; spaces>0; spaces--){
+		s=s+" ";
+	    }
+	    for (stars=( 1+2*(row -1)); stars>0; stars--){
+		s=s+"*";
+	    }
+	    s=s+"\n";
+        
+	}
+	   
+	
+	return s;
+    }
+
+    public String diamond(int h) {
+	String s="";
+	int row,spaces,stars;
+
+	for (row = 1; row <=h; row++){
+	    if (row <= (h/2)+1){
+	    for (spaces=h-row-2; spaces>0; spaces--){
+		s=s+" ";
+	    }
+	    for (stars=(1+2*(row -1)); stars>0; stars--){
+		s=s+"*";
+        
+	    }	s+="\n";
+	    } else {
+		for (spaces=(row-(h/2)-1); spaces>0; spaces--){
+		s=s+" ";
+	    }
+	        for (stars=(2 * (h-row)+1); stars>0; stars--){
+		s=s+"*";
+		}
+		s+="\n";}
+	}
+	    
+	return s;
+    }
+
+
+
+
+
+
+public String tri4(int a) {
+	int rowcounter;
+	int starcounter;
+	int spacecounter;
+	String output="";
+
+	for(rowcounter=1;rowcounter<=a;rowcounter++){
+	    for(spacecounter=rowcounter-1;spacecounter>0;spacecounter--){
+		output= output + " ";
+
+		 }
+	    for(starcounter=a-rowcounter+1;starcounter>0;starcounter--){
+	    
+		    output= output+ "*";
+		}
+	    
+		    output=output+"\n";
+	}
+	return output;
+}}
+
+
+	     
