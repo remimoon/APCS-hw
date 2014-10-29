dc9503a6800c91bfaf34abbc7eede3d3d1e84702
remimoon <remim00n7@gmail.com>
2014-10-29 09:15:09 -0400

    dfa

--- 14-arrayStuff/Driver.java
+++ 14-arrayStuff/Driver.java
@@ -1,7 +1,7 @@
 public class Driver {
     public static void main (String[] arg){
 	int[] n = {1,2,3,4,5};
-	x = new Arraystuff[n];
+	Arraystuff x = new Arraystuff(n);
 	System.out.println(""+ x.maxVal());
 	System.out.println(""+x.find(2));
     }
