abb8fd48f44a64183080eca79e4f8e47bd954b45
remimoon <remim00n7@gmail.com>
2014-10-29 19:57:24 -0400

    Create Arraystuff.java

---
+++ 14-arrayStuff/Arraystuff.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class  Arraystuff {
+    private int[] a;
+
+public Arraystuff(int[] a) {
+	this.a = a;
+    }
+
+    /*   public Arraystuff(){
+	this.n = n;
+	} */
+    
+	//  int find(n) - returns the index of the first occurence of n in the array or -1 if n isn't in the array.
+	// int maxVal() - return the largest value in the array
+
+    //code keeps saying variable x might not be intialized, not sure why
+	
+public int maxVal (){
+    int x = a[0];
+    for (int i = 0; i < a.length; i++){
+        if (a[i] > x ){
+	    x = a[i];
+	}
+	break;
+  }  
+    return x;
+    
+}
+
+
+
+public int find (int n){
+    int x = -1;
+    for (int i = 0; i < a.length; i++){
+        if (a[i] == n ){
+	    x=i;
+	}  
+	break;
+    }
+    return x;
+}
+
+    public int freq(int n){
+	int times = 0;
+	for (int i = 0; i < a.length; i++){
+	    if (a[i] == a[n]){
+		times += 1;
+	    }
+	}
+	return times;
+
+}
+
+
+}
+
+
