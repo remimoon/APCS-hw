a7037b677fc283db040a093a577d5620ff6ae345
Remi Moon <remim00n7@gmail.com>
2014-10-31 00:12:33 -0400

    hw14b

--- 14-arrayStuff/practice.java
+++ 14-arrayStuff/practice.java
@@ -36,3 +36,24 @@ public boolean more14(int[] nums) {
 	return false;
     } 
 }
+
+public int[] seriesUp(int n) {
+	int place=0;
+	int[] s = new int[n*(n+1)/2];
+	for(int i=0; i<=n; i++){
+	    for(int j=1; j<=i; j++){
+		s[place] = j;
+		place += 1;
+	    }
+	}
+	return s;
+    }
+
+maxmirror??? very confused
+
+    public int maxMirror(int[] nums) {
+	if(nums.length==0) return 0;
+	int output=1;
+	for(int i=0; i<nums.length; i++){
+	    for(int x=i+1; x<nums.length; x++){
+		if(nums[i]==nums[x]){
