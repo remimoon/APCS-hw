f0638d8192317d0e1661dbcafa539c0bb0d4253f
remimoon <remim00n7@gmail.com>
2014-11-05 09:26:03 -0500

    mode

--- 14-arrayStuff/Driver.java
+++ 14-arrayStuff/Driver.java
@@ -1,8 +1,9 @@
 public class Driver {
     public static void main (String[] arg){
 	int[] n = {1,2,3,4,5};
-	Arraystuff x = new Arraystuff(n);
-	System.out.println(""+ x.maxVal());
-	System.out.println(""+x.find(2));
+	mode x = new mode(n);
+	//	System.out.println(""+ x.maxVal());
+	//	System.out.println(""+x.find(2));
+	System.out.println(x);
     }
 }
