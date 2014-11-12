9971d90af3bc19e4faac164172ce86d8893e4ed8
Remi Moon <remim00n7@gmail.com>
2014-11-11 21:20:18 -0500

    updating Sarray, unsure about catch

--- 15-Sarray/Sarray.java
+++ 15-Sarray/Sarray.java
@@ -1,11 +1,10 @@
 public class Sarray {
     int[] data;
     int   last;
-
     public Sarray() {
         // set up the initial instance variables
 	data = new int[10];
-	last = 5;
+	last = 0;
     }
 
     public boolean add(int i){
@@ -25,63 +24,117 @@ public class Sarray {
     }
 
 
-    public void  add(int index, int i){
+    public boolean add(int index, int i){
         // adds item i  at index, shifting everything down as needed.
         // also grows as needed
-	if (data.length > size()){
-	    int[]old = int[data.length];
-	    old[]=data[];
+	 int x = data.length;
+	if (x > size()){
+	    int[] old = new int[x];
+	    for (int b = 0; b < x; b++){
+		old[b] = data[b];
+	    }
 	    data[index] = i;
-	    for (int a = 1; a < old.length - index; a++;){
+	    for (int a = index; a < old.length; a++){
 		data[index + a] = old[index + a];
 	    }
 	}else {
-	    int[] added = new int[data.length+1];
-	    for (int b = 0; index < data.index; b++){
+	    int[] added = new int[x+1];
+	    for (int b = 0; index < x; b++){
 		added[b] = data[b];
 	    }
 	    data[index]=i;
-	    for (int c = index+ 1; c < data.length; c++){
+	    for (int c = index+ 1; c < x; c++){
 	    added[c] = data[c-1];
 	    }
 	}
 	return true;
-	}
     }
+
 
     public int size() {
         // returns the number of items in the list (not the array size)
-	int index = 0;
-	searchFromEnd:
-	for (int i = data.length; i>0 ; i--){
-	    if (data[i] != 0) {
-		index = i+1;
-		break searchFromEnd;
-	    }
-	}
-	return index;
+	return data.length;
     }
+
 
     public int get(int index) {
         // returns the item at location index of the list
-	return data[index];
+
+	/*
+	try {
+            if (index > (size()-1) ) {
+                this.niceError();
+            }
+	*/
+            return data[index];
+        }
+    /*
+        catch (ArrayIndexOutOfBounds a) {
+            System.out.println("this array index is not in bounds " + a);
+        }
+        catch (IndexOutOfBounds i) {
+            System.out.println("this index is not in bounds " + i);
+        }
+
     }
+    */
 
     public int set(int index, int i){
         // sets the item at location index to value i
         // returns the old value.
+
+	/*
+	try{
+	    if (index > (size()-1)) {
+		this.niceError();
+		    }
+	*/
 	int old = data[index];
 	data[index] = i;
 	return old;
+	}
+    /*
+	catch (ArrayIndexOutOfBounds a) {
+            System.out.println("this array index is not in bounds " + a);
+        }
+        catch (IndexOutOfBounds i) {
+            System.out.println("this index is not in bounds " + i);
+        }
+
     }
+    */
 
     public int remove(int index){
         // removes the item at index i
         // returns the old value
+
+	/*
+	try{
+    	if (index > (size()-1)){
+	    this.niceError();
+		}
+	*/
 	int old = data[index];
-	for (int i = index; i < size(); i++){
-	    data[i] = data[i+1];
+	int[]removed = new int[data.length - 1];
+	for (int i = 0; i < index; i++){
+	    removed[i] = data[i];
+	}
+	for (int j = index; j < data.length; j++){
+	    removed[j-1] = data[j];
 	}
 	return old;
+	}
+    /*
+	catch (ArrayIndexOutOfBounds a) {
+            System.out.println("this array index is not in bounds " + a);
+        }
+        catch (IndexOutOfBounds i) {
+            System.out.println("this index is not in bounds " + i);
+        }
+    }
+    */
+
+ public void niceError() {
+        System.out.println("nice error message :)");
     }
 }
