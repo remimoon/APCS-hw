b6f23c95a6e39762efd556a02283c486a7950d53
Remi Moon <remim00n7@gmail.com>
2014-11-13 00:15:36 -0500

    adding hw

---
+++ 16-ArrayLists/Driver.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+
+	static ArrayList<Integer> ai = new ArrayList<Integer>();
+
+    public static void randomArrayy() {
+
+	Random r = new Random();
+	for (int i = 0; i < 20; i++) {
+	    ai.add(r.nextInt(5));
+	}
+	System.out.println("w/ duplicates: " + ai);
+    }
+
+
+    public static void main(String[] args) {
+	randomArrayy();
+	for (int i = 0; i < ai.size()-1; i++){
+	    if ((ai.get(i) == ai.get(i+1)) || (ai.get(i) == ai.get(i-1))){
+		ai.remove(i);	
+	    }
+	}	
+	System.out.println("w/out duplicates: " + ai);
+    }
+}
