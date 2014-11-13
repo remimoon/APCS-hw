59c024007253414f461935c618d5b849522a8683
remimoon <remim00n7@gmail.com>
2014-11-13 08:54:39 -0500

    adding comment to hw

--- 16-ArrayLists/Driver.java
+++ 16-ArrayLists/Driver.java
@@ -6,7 +6,6 @@ public class Driver {
 	static ArrayList<Integer> ai = new ArrayList<Integer>();
 
     public static void randomArrayy() {
-
 	Random r = new Random();
 	for (int i = 0; i < 20; i++) {
 	    ai.add(r.nextInt(5));
@@ -18,6 +17,14 @@ public class Driver {
     public static void main(String[] args) {
 	randomArrayy();
 	for (int i = 0; i < ai.size()-1; i++){
+	    /*
+~~~~another way~~~~~
+	      if ((ai.get(i) == ai.get(i+1)){
+		ai.remove(i);
+		i--;
+		  }
+	    */
+
 	    if ((ai.get(i) == ai.get(i+1)) || (ai.get(i) == ai.get(i-1))){
 		ai.remove(i);	
 	    }
