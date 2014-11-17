f2b38339c13fb4b8b2a4abae004fa3f936b15d21
Remi Moon <remim00n7@gmail.com>
2014-11-17 00:11:50 -0500

    cant figure it out

--- 17-ArrayLists/Driver.java
+++ 17-ArrayLists/Driver.java
@@ -3,7 +3,27 @@ import java.util.*;
 
 public class Driver {
 
+    static Random r = new Random();
+    
+    static ArrayList<Integer> ai = new ArrayList<Integer>();
+    static ArrayList<Integer> randomai = new ArrayList<Integer>();
 
 
+    public static void ai(){
+    for ( int i = 0; i < 20; i++) {
+	ai.add(i);
+    }
 
+    System.out.println(ai);
+    }
 
+    public static void main(String[] args) {
+	ai();
+	for (int i = ai.size(); i > 1; i--){
+	    int random = r.nextInt(ai.size());
+	    randomai.set(i,ai.get(random));
+	    ai.remove(ai.indexOf(random));
+	}
+	System.out.print(randomai);
+    }
+}
