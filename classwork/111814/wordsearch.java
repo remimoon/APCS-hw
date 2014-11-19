6626850c6fd014b73407f8168f8c73208dec359e
remimoon <remim00n7@gmail.com>
2014-11-19 09:02:36 -0500

    updating wordsearch

--- classwork/111814/wordsearch.java
+++ classwork/111814/wordsearch.java
@@ -5,7 +5,7 @@ public class wordsearch {
 	board = new char[r][c];
 	for (int i = 0; i < board.length; i++){
 	    for (int j = 0; j < board[i].length; j++){
-		board[i][j] = '.';
+		board[i][j] ='.';
 	    }
 	}
     }
@@ -13,15 +13,32 @@ public class wordsearch {
     public wordsearch(){
 	this(20,40);
     }
+
     public String toString(){
-	String s = " ";
+	String s = "";
 	for (int i = 0; i < board.length; i++){
-	    for (int j = 0; j < board[i].length; i++){
-		s = s+board[i][j];
+	    for (int j = 0; j < board[i].length; j++){
+		s = s + board[i][j];
 	    }
 	    s = s + "\n";
 	}
 	return s;
+    }
+
+    public void addwordH(String w, int row, int col){
+	for (int i = 0; i < w.length(); i++){
+	    board[row][col] = w.chatAr(i);
+	    c++;
+	}
+    }
+
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	System.out.println(w);
+	w.addWordH("hello",3,15); //should work 
+	w.addWordH("look",3,14); //test illegal overlap
+	w.addWordH("look",3,10); //test legal overlap				
+	System.out.println(w);
     }
 
 }
