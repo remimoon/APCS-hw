b7004c64427687a773e1afd4a645c065104c8dd7
remimoon <remim00n7@gmail.com>
2014-12-01 09:26:46 -0500

    Update OSA.java

--- 19-OrderedSuperArray/OSA.java
+++ 19-OrderedSuperArray/OSA.java
@@ -1,18 +1,6 @@
 public class OSA extends SuperArray{
 
-    public String[] Order(){
-	String [] ordered = new String[data.length];
-	for(int j=0; j < ordered.length; j++){
-	    for (int i=j+1 ; i< ordered.length; i++){
-		if(Array[i].compareTo(Array[j])<0){
-		    String temp= ordered[j];
-		    Array[j]= ordered[i];
-		    Array[i]=temp;
-		}
-	    }
-	}
-	return ordered[];
-    }
+
     
     public boolean add(String i){
 
