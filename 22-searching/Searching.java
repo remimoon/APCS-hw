c60d525bf3da9e2e66cb37b1c9e7bceef3d49f0e
remimoon <remim00n7@gmail.com>
2014-12-11 09:27:17 -0500

    sdfas

---
+++ 22-searching/Searching.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.util.*;
+
+
+public class Searching implements Comparable{
+
+    private Comparable[] a;
+
+    public Searching(){
+	a = new Searching[10];
+    }
+
+    public Searching (int len){
+	a = new Searching[len];
+    }
+
+    public void additem(String s)
